2dbbcbdca4b7d779ce31e4251785f4cb2ab1d850
letsgetrusty <bogdan@letsgetrusty.com>
2024-07-19 09:42:06 +0200

    added config file to load_balancer to store worker host addresses

--- load_balancer/src/main.rs
+++ load_balancer/src/main.rs
@@ -1,15 +1,31 @@
+use config::Config;
 use load_balancer::{run_server, DecisionEngine, LoadBalancer, MetricsClient, RoundRobinStrategy};
+use serde::Deserialize;
 use std::{net::SocketAddr, sync::Arc, time::Duration};
 use tokio::sync::RwLock;
 
+#[derive(Debug, Deserialize)]
+#[allow(unused)]
+struct ConfigSettings {
+    worker_hosts: Vec<String>,
+}
+
 #[tokio::main]
 async fn main() {
-    let worker_hosts = vec![
-        "http://localhost:49961".to_string(),
-        "http://localhost:49962".to_string(),
-    ];
+    let config = Config::builder()
+        // Add in `/config.json`
+        .add_source(config::File::with_name("config"))
+        // Add in settings from the environment (with a prefix of LB)
+        // Eg.. `LB_DEBUG=1 ./target/app` would set the `debug` key
+        .add_source(config::Environment::with_prefix("APP"))
+        .build()
+        .unwrap();
 
-    let strategy = Arc::new(RwLock::new(RoundRobinStrategy::new(worker_hosts.clone())));
+    let config_settings: ConfigSettings = config.try_deserialize().unwrap();
+
+    let strategy = Arc::new(RwLock::new(RoundRobinStrategy::new(
+        config_settings.worker_hosts.clone(),
+    )));
     let metrics_client = Arc::new(MetricsClient::new());
     let load_balancer = Arc::new(RwLock::new(LoadBalancer::new(
         strategy,
