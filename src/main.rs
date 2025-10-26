b47be5818e3ea54ca1e75dd07874760bd28cc1bd
Bogdan <bogdan@letsgetrusty.com>
2025-10-26 19:37:19 +0100

    feat: use latest version of tokio, hyper, and tower

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::{convert::Infallible, net::SocketAddr, str::FromStr, sync::Arc};
+use std::{net::SocketAddr, str::FromStr, sync::Arc};
 
-use hyper::{
-    client::ResponseFuture,
-    service::{make_service_fn, service_fn},
-    Body, Client, Request, Response, Server, Uri,
-};
+use hyper::{body::Incoming, service::service_fn, Request, Response, Uri};
+use hyper::server::conn::http1;
+use hyper_util::client::legacy::connect::HttpConnector;
+use hyper_util::client::legacy::{Client, Error as ClientError, ResponseFuture};
+use hyper_util::rt::{TokioExecutor, TokioIo};
 use tokio::sync::RwLock;
+use tokio::{net::TcpListener, task};
 
 struct LoadBalancer {
-    client: Client<hyper::client::HttpConnector>,
+    client: Client<HttpConnector, Incoming>,
     worker_hosts: Vec<String>,
     current_worker: usize,
 }
@@ -19,14 +20,17 @@ impl LoadBalancer {
             return Err("No worker hosts provided".into());
         }
 
+        let connector = HttpConnector::new();
+        let client = Client::builder(TokioExecutor::new()).build(connector);
+
         Ok(LoadBalancer {
-            client: Client::new(),
+            client,
             worker_hosts,
             current_worker: 0,
         })
     }
 
-    pub fn forward_request(&mut self, req: Request<Body>) -> ResponseFuture {
+    pub fn forward_request(&mut self, req: Request<Incoming>) -> ResponseFuture {
         let mut worker_uri = self.get_worker().to_owned();
 
         // Extract the path and query from the original request
@@ -64,9 +68,9 @@ impl LoadBalancer {
 }
 
 async fn handle(
-    req: Request<Body>,
+    req: Request<Incoming>,
     load_balancer: Arc<RwLock<LoadBalancer>>,
-) -> Result<Response<Body>, hyper::Error> {
+) -> Result<Response<Incoming>, ClientError> {
     load_balancer.write().await.forward_request(req).await
 }
 
@@ -83,12 +87,21 @@ async fn main() {
 
     let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 1337));
 
-    let server = Server::bind(&addr).serve(make_service_fn(move |_conn| {
-        let load_balancer = load_balancer.clone();
-        async move { Ok::<_, Infallible>(service_fn(move |req| handle(req, load_balancer.clone()))) }
-    }));
+    let listener = TcpListener::bind(addr)
+        .await
+        .expect("failed to bind TCP listener");
 
-    if let Err(e) = server.await {
-        println!("error: {}", e);
+    loop {
+        let (stream, _) = listener.accept().await.expect("failed to accept");
+        let load_balancer = load_balancer.clone();
+
+        task::spawn(async move {
+            let io = TokioIo::new(stream);
+            let service = service_fn(move |req| handle(req, load_balancer.clone()));
+
+            if let Err(e) = http1::Builder::new().serve_connection(io, service).await {
+                eprintln!("error: {}", e);
+            }
+        });
     }
 }
